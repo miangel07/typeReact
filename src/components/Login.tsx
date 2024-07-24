8a7b1f0b5ce923abf3ec4daa9f405183bbad2f63
miangel07 <miguelosoriorojas063@gmail.com>
2024-07-23 21:41:10 -0500

    style login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,8 @@ import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "@tanstack/react-router";
+import img from "../../public/logo-sena-verde.jpg";
+
 type Response = {
   username: string;
   password: string;
@@ -21,7 +23,7 @@ const LoginComponen = () => {
     mutationFn: login,
     onSuccess: (data) => {
       console.log("Usuario autorizado", data);
-      navigate("/home")
+      navigate({ to: "/home" });
     },
     onError: (error: Error) => {
       console.log("Error:", error.message);
@@ -40,25 +42,27 @@ const LoginComponen = () => {
 
   return (
     <>
-      <div className="flex   flex-col h-screen items-center">
-        <div className="flex flex-col  justify-center w-full items-center bg-gray-100 ">
-          <h1 className="text-3xl mt-3">Coffco</h1>
-          <p>Ingresa tus credenciales</p>
+      <div className="flex w-full  flex-col  items-center">
+        <div className="flex flex-col  justify-start w-full pl-4 pt-4  ">
+          <div
+            className="w-[85px] h-[85px] bg-cover bg-center"
+            style={{ backgroundImage: `url(${img})` }}
+          ></div>
         </div>
-        <div className=" w-[450px] mt-48 justify-center flex h-[450px] items-center shadow-2xl rounded-lg">
+        <div className=" w-[450px] mt-48  flex h-[450px] items-center shadow-2xl rounded-lg">
           <form
-            className=" flex flex-col gap-2 md:w-[450px] h-[350px] w-fit justify-between  items-center"
+            className=" flex flex-col gap-2 md:w-[450px] h-[350px] w-fit justify-between  pl-4"
             onSubmit={handleSubmit(onsubmitData)}
           >
             <div className="w-full flex justify-center ">
-              <h1 className="text-xl font-bold">Login</h1>
+              <h1 className="text-xl font-bold">Coffco</h1>
             </div>
             {errorApi}
             <label>Nombre Usuario:</label>
 
             <input
               className="text-black  h-[36px] rounded-lg  outline outline-3 outline-none  pl-3
-        border-solid border-2 w-80"
+        border-solid border-2 w-96"
               type="text"
               id="username"
               placeholder="Ejemplo Miguel07"
@@ -77,7 +81,7 @@ const LoginComponen = () => {
 
             <input
               type="password"
-              className="text-black w-80 h-[36px] rounded-lg  outline outline-3 outline-none  pl-3
+              className="text-black w-96 h-[36px] rounded-lg  outline outline-3 outline-none  pl-3
         border-solid border-2 "
               id="password"
               {...register("password", {
